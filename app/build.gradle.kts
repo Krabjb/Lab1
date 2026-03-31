5cae8a380f3b81c1ecc05ef9d90b47057c019265
traceear <traceear@gmail.com>
2026-03-31 20:43:22 +0600

    last commit. Everything is ready

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,7 @@ plugins {
 
 android {
     namespace = "ru.omgtu.lab1"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "ru.omgtu.lab1"
---
+++ app/src/main/java/ru/omgtu/lab1/BlankFragment.java
@@ -0,0 +1,22 @@
+package ru.omgtu.lab1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class BlankFragment extends Fragment {
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first_blank, container, false);
+    }
+}
